1659101ccb7573e0c4a013fb19fe30ddcd7171a5
Alec Linyard <alec.linyard@gmail.com>
2019-10-24 08:31:46 -0700

    added .h file

---
+++ AlgorithmLab/AlgorithmLab/main.h
@@ -0,0 +1,13 @@
+//
+//  main.h
+//  AlgorithmLab
+//
+//  Created by Alec Linyard on 10/24/19.
+//  Copyright © 2019 Alec Linyard. All rights reserved.
+//
+
+#ifndef main_h
+#define main_h
+
+
+#endif /* main_h */
