59a1891a7493976ae56661d41f77563fc2152b2e
Alec Linyard <alec.linyard@gmail.com>
2019-10-24 08:31:06 -0700

    creating Xcode project

---
+++ AlgorithmLab/AlgorithmLab/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AlgorithmLab
+//
+//  Created by Alec Linyard on 10/24/19.
+//  Copyright © 2019 Alec Linyard. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
