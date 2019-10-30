dcbd972c800711ab939b0efc2b261adb2d99c8f5
gwen <gavestal@gmail.com>
2019-10-30 07:47:17 -0700

    code

--- AlgorithmLab/AlgorithmLab/main.c
+++ AlgorithmLab/AlgorithmLab/main.c
@@ -8,8 +8,25 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
+
+struct languages
+{
+    char name[100];
+    char birthdate[100];
+    char executiontype[100];
+    char languagetype[100];
+};
+
+struct languages MyLanguages =
+    {
+        {"Python", "1989", "Jit Interpreted", "MP"},
+        {"JavaScript", "1995", "Jit Interpreted", "MP"},
+        {"Java", "1990", "JVM Bytecode", "OOP"},
+        {"Kotlin", "2016", "JVM Bytecode", "MP"},
+        {"Swift", "2014", "Native code", "OOP"},
+        {"Objective C", "1980", "Native code","OOP"},
+        {"C++", "1979", "Native code","OOP"},
+        {"C", "1972", "Native code", "Iterative"},
+        {"C#", "2000", "IL Bytecode","MP"},
+        {"Ruby", "1990", "YARV Bytecode","OOP"},
+    };
